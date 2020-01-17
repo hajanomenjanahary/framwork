1b35a5059a4f6259f4c32ae634ac26675b5f004d
angelopareto98 <angelopareto98@gmail.com>
2020-01-17 13:14:51 +0100

    Pareto

---
+++ src/Entity/Commande.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CommandeRepository")
+ */
+class Commande
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date_com;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateCom(): ?\DateTimeInterface
+    {
+        return $this->date_com;
+    }
+
+    public function setDateCom(\DateTimeInterface $date_com): self
+    {
+        $this->date_com = $date_com;
+
+        return $this;
+    }
+}
